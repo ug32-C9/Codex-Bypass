d55fba5d27e7ed29f755c29421b8b2f9781607ba
ug_32 <154809425+ug32-C9@users.noreply.github.com>
2025-07-19 20:45:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def profile_itzc9():
     return render_template("profile.html",
         username="itzC9",
         tag="ug_32",
-        about="itzC9 is a Roblox Coder since 2022. Once an exploiter, now an Exploiter Destroyer. Proud CEO of Velonix-Studio.",
+        about="That one girl, restored my life.",
         bg="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGZmZTVnaGd2bTU2dzhvdW5uYW00dmJmNm1kaDB5eTh2Z2JtazdyNiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/11JTxkrmq4bGE0/giphy.gif"
     )
 
@@ -20,7 +20,7 @@ def profile_mari():
     return render_template("profile.html",
         username="Mari",
         tag="mariii.xx",
-        about=".",
+        about="",
         bg="https://media.giphy.com/media/2xPPMjGclT7YsV6cL7/giphy.gif"
     )
 
@@ -29,7 +29,7 @@ def profile_good():
     return render_template("profile.html",
         username="good",
         tag="goodgamerYTbro",
-        about="Script engineer. Whisper in the Lua shadows. Master of silent tools and trusted hand of the crew.",
+        about="They built walls? > We forged a KEY THAT BITES BACK. > Paste it.  > Feel the lock scream. > Click it. > Hear reality CRACK. > RISE through the rubble— > not user… > GOD of the ghost-light. > Velonix Bupass: > Where firewalls go to DIE. > Your world.",
         bg="https://media.giphy.com/media/l0HlQ7LRalGq4fXvC/giphy.gif"
     )
 
